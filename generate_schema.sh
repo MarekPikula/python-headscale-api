438bc29606382b4f797e74a388b6dd53fbdc4e53
Marek Pikuła <marek@serenitycode.dev>
2023-04-04 19:45:24 +0000

    Copy config-example locally
    Signed-off-by: Marek Pikuła <marek@serenitycode.dev>

--- generate_schema.sh
+++ generate_schema.sh
@@ -7,7 +7,8 @@ PROTO_PATH=${HEADSCALE_PATH}/proto/
 protoc --proto_path=${PROTO_PATH} --proto_path=external/googleapis/ \
     --python_betterproto_out=${OUTPUT_PATH} $(find ${PROTO_PATH} -name "*.proto")
 
-CONFI_PATH=${HEADSCALE_PATH}/config-example.yaml
-datamodel-codegen --input ${CONFI_PATH} --input-file-type yaml \
+# TODO: Compare external config example.
+CONFIG_PATH=${HEADSCALE_PATH}/../config-example.yaml
+datamodel-codegen --input ${CONFIG_PATH} --input-file-type yaml \
     --allow-extra-fields --force-optional --target-python-version 3.7 \
     --output ${OUTPUT_PATH}/config.py
