39acb65432129096db2d90e21a629f2797190f80
Marek Pikuła <marek@serenitycode.dev>
2023-04-04 15:34:14 +0000

    Add basic Headscale API
    Signed-off-by: Marek Pikuła <marek@serenitycode.dev>

--- headscale_api/__init__.py
+++ headscale_api/__init__.py
@@ -1,1 +1,13 @@
 """Headscale API module."""
+
+from .headscale import Headscale
+
+if __name__ == "__main__":
+    import asyncio
+    import os
+
+    headscale = Headscale(
+        os.getenv("HEADSCALE_APIURL", "localhost:5000"),
+        api_key=os.getenv("HEADSCALE_APIKEY", None),
+    )
+    print(asyncio.run(headscale.get_user(name="marek")))
---
+++ headscale_api/headscale.py
@@ -0,0 +1,143 @@
+"""Headscale API abstraction."""
+
+__authors__ = ["Marek Pikuła <marek@serenitycode.dev>"]
+
+import logging
+from dataclasses import dataclass
+from json import JSONDecodeError
+from typing import Any, Dict, List, Optional, Type, TypeVar, Union
+
+import requests
+from betterproto import Message
+
+from .endpoints import ENDPOINTS, Endpoint
+from .schema.headscale import v1 as model
+
+
+@dataclass
+class ErrorResponse(RuntimeError):
+    """Error response from Headscale."""
+
+    code: int
+    message: str
+    details: List[str]
+
+    def __str__(self) -> str:  # noqa
+        return f"Response (code {self.code}): {self.message}"
+
+
+MessageT = TypeVar("MessageT", bound=Message)
+"""Message type for Headscale._unary_unary() function."""
+
+
+class Headscale(model.HeadscaleServiceStub):
+    """Headscale API abstraction."""
+
+    def __init__(  # pylint: disable=super-init-not-called
+        self,
+        base_url: str,
+        api_key: Optional[str] = None,
+        requests_timeout: float = 10,
+        logger: Union[logging.Logger, int] = logging.INFO,
+    ):
+        """Initialize Headscale API.
+
+        Arguments:
+            base_url -- base API URL (without `/api/v1`).
+
+        Keyword Arguments:
+            api_key -- API key, which can be overriden later (default: {None})
+            requests_timeout -- request timeout in seconds (default: {10})
+            logger -- logger to use or default logging level
+                (default: {logging.INFO})
+        """
+        self._base_url = base_url
+        self.api_key = api_key
+        self.timeout = requests_timeout
+        if isinstance(logger, logging.Logger):
+            self._logger = logger
+        else:
+            logging.basicConfig(level=logger)
+            self._logger = logging.getLogger(__name__)
+
+    async def _unary_unary(
+        self,
+        route: str,
+        request: Message,
+        response_type: Type[MessageT],
+        *,
+        timeout: Optional[Any] = None,
+        deadline: Optional[Any] = None,
+        metadata: Optional[Any] = None,
+    ) -> MessageT:
+        """Execute an unary operation on the API.
+
+        Used by HeadscaleServiceStub functions.
+        """
+        try:
+            endpoint = ENDPOINTS[route]
+            assert isinstance(endpoint, Endpoint)
+        except KeyError as error:
+            raise NotImplementedError(
+                f'Route "{error.args[0]}" not supported. Contact the module maintainer.'
+            ) from error
+
+        request_dict: Dict[str, Any] = request.to_dict()  # type: ignore
+        self._logger.info(endpoint.logger_start_message.format_map(request_dict))
+
+        api_url = endpoint.api_url.format_map(request_dict)
+        response = requests.request(
+            endpoint.request_type,
+            f"{self._base_url}{api_url}",
+            params=request_dict,
+            headers={
+                "Accept": "application/json",
+                "Authorization": f"Bearer {self.api_key}",
+            },
+            timeout=self.timeout,
+        )
+
+        def error_message():
+            message = (
+                endpoint.logger_fail_message.format_map(request_dict)
+                if endpoint.logger_fail_message is not None
+                else f'Request to "{api_url}" failed.'
+            ) + f" ({response.status_code})"  # type: ignore
+            self._logger.error(message)
+            return message
+
+        if response.status_code != 200:
+            error_message()
+            try:
+                raise ErrorResponse(**response.json())
+            except JSONDecodeError as error:
+                raise ErrorResponse(
+                    response.status_code, response.content.decode(), []
+                ) from error
+
+        try:
+            response_dict = response.json()
+            assert isinstance(response_dict, dict)
+            response_parsed = response_type().from_dict(response_dict)  # type: ignore
+        except (JSONDecodeError, AssertionError, ValueError) as error:
+            raise ErrorResponse(response.status_code, error_message(), []) from error
+
+        if endpoint.logger_success_message is not None:
+            self._logger.info(
+                endpoint.logger_success_message.format_map(
+                    dict(request_dict, **response_dict)
+                )
+            )
+        return response_parsed
+
+    async def _unary_stream(  # type: ignore
+        self,
+        route: str,
+        request: Any,
+        response_type: Any,
+        *,
+        timeout: Optional[float] = None,
+        deadline: Optional[Any] = None,
+        metadata: Optional[Any] = None,
+    ) -> Any:
+        raise NotImplementedError("Stream operation not implemented.")
